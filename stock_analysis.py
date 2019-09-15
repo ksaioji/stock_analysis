abb91737be7accde5f32322e5276b782f2f6a0be
ksaioji <kiyonori.saioji@ipg.co.jp>
2019-09-15 14:48:26 +0900

    tidying code, putting some codes in def.

--- stock_analysis.py
+++ stock_analysis.py
@@ -1,8 +1,22 @@
+import sys
 import pandas as pd
 import datetime
 import pandas_datareader.data as web
 from pandas import Series, DataFrame
 
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+##### Adjusting the size of matplotlib #####
+import matplotlib as mpl
+#mpl.rc('figure', figsize=(8, 7))
+mpl.rc('figure', figsize=(10, 10))
+mpl.__version__
+
+# Adjusting the style of matplotlib
+style.use('ggplot')
+
+##### Apple data loading #####
 start = datetime.datetime(2010, 1, 10)
 #end = datetime.datetime.now()
 end = datetime.datetime(2017, 1, 11)
@@ -13,47 +27,7 @@ df.tail()
 close_px = df['Adj Close']
 mavg = close_px.rolling(window=100).mean()
 
-import matplotlib.pyplot as plt
-from matplotlib import style
-
-# Adjusting the size of matplotlib
-import matplotlib as mpl
-mpl.rc('figure', figsize=(8, 7))
-mpl.__version__
-
-# Adjusting the style of matplotlib
-style.use('ggplot')
-
-close_px.plot(label='AMZN')
-mavg.plot(label='mavg')
-plt.legend()
-plt.show()
-
-import matplotlib.pyplot as plt
-from matplotlib import style
-
-# Adjusting the size of matplotlib
-import matplotlib as mpl
-#mpl.rc('figure', figsize=(8, 7))
-mpl.rc('figure', figsize=(15, 9))
-mpl.__version__
-
-# Adjusting the style of matplotlib
-style.use('ggplot')
-
-close_px.plot(label='AAPL')
-mavg.plot(label='mavg')
-plt.legend()
-plt.show()
-
-# return deviation
-rets = close_px / close_px.shift(1) - 1
-rets.head()
-
-rets.plot(label='return')
-plt.show()
-
-# analysing competitor stocks
+##### analysing competitor stocks #####
 dfcomp = web.DataReader(['AAPL', 'GE', 'GOOG', 'IBM', 'MSFT'],'yahoo',start=start,end=end)['Adj Close']
 dfcomp.tail()
 
@@ -62,41 +36,62 @@ retscomp = dfcomp.pct_change()
 corr = retscomp.corr()
 corr.tail()
 
-# apple and GE return distribution
-plt.scatter(retscomp.AAPL, retscomp.GE)
-plt.xlabel('Returns AAPL')
-plt.ylabel('Returns GE')
-plt.show()
+def show_aapl():
+    close_px.plot(label='AAPL')
+    mavg.plot(label='mavg')
+    plt.legend()
+    plt.show()
 
-# Kernel Density Estimate (KDE)
-pd.plotting.scatter_matrix(retscomp, diagonal='kde', figsize=(10, 10));
-plt.show()
+def deviation():
+    # return deviation
+    rets = close_px / close_px.shift(1) - 1
+    print(rets)
+    
+    rets.plot(label='return')
+    plt.show()
+    
+def apple_ge_distribution():    
+    # apple and GE return distribution
+    plt.scatter(retscomp.AAPL, retscomp.GE)
+    plt.xlabel('Returns AAPL')
+    plt.ylabel('Returns GE')
+    plt.show()
 
-# heat map
-plt.imshow(corr, cmap='hot', interpolation='none')
-plt.colorbar()
-plt.xticks(range(len(corr)), corr.columns)
-plt.yticks(range(len(corr)), corr.columns);
-plt.show()
+def kde():
+    # Kernel Density Estimate (KDE)
+    pd.plotting.scatter_matrix(retscomp, diagonal='kde', figsize=(10, 10));
+    plt.show()
 
-# stock returns rate and risk
-plt.scatter(retscomp.mean(), retscomp.std())
-plt.xlabel('Expected returns')
-plt.ylabel('Risk')
-for label, x, y in zip(retscomp.columns, retscomp.mean(), retscomp.std()):
-    plt.annotate(
-        label, 
-        xy = (x, y), xytext = (20, -20),
-        textcoords = 'offset points', ha = 'right', va = 'bottom',
-        bbox = dict(boxstyle = 'round,pad=0.5', fc = 'yellow', alpha = 0.5),
-        arrowprops = dict(arrowstyle = '->', connectionstyle = 'arc3,rad=0'))
-plt.show()
+def heat_map():
+    # heat map
+    plt.imshow(corr, cmap='hot', interpolation='none')
+    plt.colorbar()
+    plt.xticks(range(len(corr)), corr.columns)
+    plt.yticks(range(len(corr)), corr.columns);
+    plt.show()
+
+def stock_returns_rate_risk():
+    # stock returns rate and risk
+    plt.scatter(retscomp.mean(), retscomp.std())
+    plt.xlabel('Expected returns')
+    plt.ylabel('Risk')
+    for label, x, y in zip(retscomp.columns, retscomp.mean(), retscomp.std()):
+        plt.annotate(
+            label, 
+            xy = (x, y), xytext = (20, -20),
+            textcoords = 'offset points', ha = 'right', va = 'bottom',
+            bbox = dict(boxstyle = 'round,pad=0.5', fc = 'yellow', alpha = 0.5),
+            arrowprops = dict(arrowstyle = '->', connectionstyle = 'arc3,rad=0'))
+    plt.show()
+    
+#stock_returns_rate_risk()
+#heat_map()
+#sys.exit()
 
 # feature engineering
 dfreg = df.loc[:,['Adj Close','Volume']]
 dfreg['HL_PCT'] = (df['High'] - df['Low']) / df['Close'] * 100.0
 dfreg['PCT_change'] = (df['Close'] - df['Open']) / df['Open'] * 100.0
-
 dfreg.head()
 
 import math
@@ -188,21 +183,28 @@ forecast_set = {
 # get last_date
 last_date = dfreg.iloc[-1].name
 
+# 余白を設定
+plt.subplots_adjust(wspace=0.4, hspace=0.8)
+
+n = 1
 for k, v in forecast_set.items():
     dfreg['Forecast'] = np.nan
     #print(dfreg['Forecast'])
     last_unix = last_date
     next_unix = last_unix + datetime.timedelta(days=1)
-    print(last_date)
+    #print(last_date)
     #for i in forecast_set:
     for i in v:
         next_date = next_unix
         next_unix += datetime.timedelta(days=1)
         dfreg.loc[next_date] = [np.nan for _ in range(len(dfreg.columns)-1)]+[i]
+    plt.subplot(3,1,n)
     dfreg['Adj Close'].tail(500).plot()
     dfreg['Forecast'].tail(500).plot()
     plt.title(k)
     plt.legend(loc=4)
     plt.xlabel('Date')
     plt.ylabel('Price')
-    plt.show()
+    n += 1
+
+plt.show()
